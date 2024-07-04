017012d484c766f680a50261c9655c1566dfa20f
sibayarn <mysibayaneduardandreimaruquez@gmail.com>
2024-07-04 15:55:34 +0800

    first commit

---
+++ js/slider.js
@@ -0,0 +1,10 @@
+var slider = tns({
+  container: ".homeBanner__slider",
+  items: 1,
+  speed: 200,
+  slideBy: "page",
+  autoplay: true,
+  navPosition: "bottom",
+  axis: "vertical",
+  controls: false,
+});
